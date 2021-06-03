71995f1a7931565f5ae1b820e6177b880bf422e4
ALazyGuy <razorprogrammer@gmail.com>
2021-06-03 10:59:11 +0300

    feat: Added sentence parser

---
+++ src/main/java/com/ltp/composite/parser/SentenceParser.java
@@ -0,0 +1,24 @@
+package com.ltp.composite.parser;
+
+import com.ltp.composite.model.ContainerToken;
+import com.ltp.composite.model.SymbolToken;
+import com.ltp.composite.model.Token;
+import com.ltp.composite.model.TokenType;
+
+public class SentenceParser extends AbstractParser{
+
+    public SentenceParser(AbstractParser next){
+        super(next);
+    }
+
+    @Override
+    public void parse(Token token, String content) {
+        String[] sentences = content.split("(?<=[!?.])\\s");
+        for(String sentence : sentences){
+            Token sentence1 = new ContainerToken(TokenType.SENTENCE);
+            next.parse(sentence1, sentence.replaceAll("[!.?]", ""));
+            sentence1.add(new SymbolToken(sentence.charAt(sentence.length() - 1)));
+            token.add(sentence1);
+        }
+    }
+}
