2cf687f8883c5f3e1cd8df3ba1fc312b18870281
ALazyGuy <razorprogrammer@gmail.com>
2021-06-04 16:49:36 +0300

    feat: integrated expression

--- src/main/java/com/ltp/composite/parser/WordParser.java
+++ src/main/java/com/ltp/composite/parser/WordParser.java
@@ -14,7 +14,7 @@ public class WordParser extends AbstractParser{
     public void parse(Token token, String content) {
         String[] words = content.split("\\s");
         for(String word : words){
-            Token token1 = new ContainerToken(TokenType.WORD);
+            Token token1 = new ContainerToken(Character.isLetter(word.charAt(0)) ? TokenType.WORD : TokenType.EXPRESSION);
             next.parse(token1, word);
             token.add(token1);
         }
